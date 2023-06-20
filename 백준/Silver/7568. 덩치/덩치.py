9b22423c91968cfd39b47a381712919896ce74a9
tallguy188 <80328668+tallguy188@users.noreply.github.com>
2023-06-20 22:25:48 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,20 @@
+n = int(input())  # 전체 사람의 수 입력받기
+people = []  # 각 사람의 덩치 정보를 저장할 리스트
+
+# 각 사람의 몸무게와 키를 입력받아 리스트에 저장
+for _ in range(n):
+    weight, height = map(int, input().split())
+    people.append((weight, height))  # 리스트에 튜플 형태로 저장
+
+ranks = []  # 덩치 등수를 저장할 리스트
+
+# 각 사람의 덩치 등수 계산
+for i in range(n):
+    rank = 1  # 초기 등수는 1로 설정
+    for j in range(n):
+        if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+            rank += 1
+    ranks.append(rank)
+
+for rank in ranks:
+    print(rank, end=' ')
