b3736a5af785b581589844ff8432426493925114
tallguy188 <80328668+tallguy188@users.noreply.github.com>
2023-06-20 22:26:41 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n,k = map(int,input().split())
+
+deq = deque()
+for i in range(n):
+    deq.append(i+1)
+
+    # [1,2,3,4,5,6,7]
+
+result = []
+
+start = 0
+while deq:      # 데크가 비어있지 않은 동안 반복을 실행
+    start = (start+k-1) % len(deq)
+    removed = deq[start]
+    deq.remove(removed)
+    result.append(str(removed))
+
+
+output = "<" + ", ".join(result) + ">"
+print(output)
+
