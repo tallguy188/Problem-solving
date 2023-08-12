1da511bab7e76ba6ab08899d01873bb3d99d8320
tallguy188 <80328668+tallguy188@users.noreply.github.com>
2023-08-12 11:45:37 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.65 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        // " " 기준으로 문자열 잘라서 배열에 넣음
+        String arr[]  = s.split(" ");
+        // 잘린 문자열을 순서대로 처리
+        for(int i=0;i<arr.length;i++){
+            String now = arr[i];
+            
+            // 문자열의 길이가 0 (공백) answer에 " "만 추가
+            if(arr[i].length() == 0){
+                answer +=  " ";    
+            }
+            // 문자가 있다면
+            else { 
+                // 0번째 문자는 대문자로 
+                answer += now.substring(0,1).toUpperCase();
+                //1번째 문자부터 마지막까지는 소문자로 
+                answer += now.substring (1,now.length()).toLowerCase();
+                answer += " ";
+            
+            }
+        }
+        
+        // 입력받은 문자열의 맨 마지막이 " " 라면 바로 answer반환
+        if(s.substring(s.length()-1,s.length()).equals(" ")) {
+            return answer;
+        }
+        
+        
+        return answer.substring(0,answer.length()-1);
+    }
+}
