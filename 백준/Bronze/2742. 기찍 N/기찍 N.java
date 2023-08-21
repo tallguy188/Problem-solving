94a3febbb1131ca7ef11ab8d754f8c26dd46579e
tallguy188 <80328668+tallguy188@users.noreply.github.com>
2023-08-21 20:42:56 +0900

    [Bronze IV] Title: 기찍 N, Time: 1140 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        for(int i=number; i>0 ;i--) {
+            System.out.println(i);
+
+        }
+    }
+
+
+}
