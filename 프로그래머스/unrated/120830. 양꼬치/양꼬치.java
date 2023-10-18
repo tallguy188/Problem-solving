f9976d7c5dfd2bf014f551f657f70993e79d1c7d
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2023-10-18 14:42:32 +0900

    [level unrated] Title: 양꼬치, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int k) {
+        
+        int middle = n/10;
+        
+        k = k-middle;
+        
+        int answer = n *12000 + k * 2000;
+        
+        
+        
+        
+        return answer;
+    }
+}
