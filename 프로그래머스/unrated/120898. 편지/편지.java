e68fe3188d0e174404b74795a18043899d6691ef
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2023-10-18 15:04:53 +0900

    [level unrated] Title: 편지, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int length = message.length();
+        answer = length*2;
+        return answer;
+    }
+}
