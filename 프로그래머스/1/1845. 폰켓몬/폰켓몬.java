1f6339e91d0e7167ab332106632e39a0e08d855c
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2024-04-19 15:32:41 +0900

    [level 1] Title: 폰켓몬, Time: 3.32 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,26 +1,24 @@
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashMap<Integer, Integer> pocket = new HashMap<>();
+        // 폰켓몬의 종류가 담길 해쉬맵 선언
+        HashMap<Integer,Integer> pocket = new HashMap<>();
         
-        // 각 폰켓몬 종류별 개수를 세기
-        for (int num : nums) {
-            pocket.put(num, pocket.getOrDefault(num, 0) + 1);
+        for(int num : nums){
+            // 해쉬맵에 종류에 따른 개수 삽입 
+            pocket.put(num,pocket.getOrDefault(num,0) + 1);
         }
         
-        // N/2마리 이내로 선택 가능한 폰켓몬 종류의 최댓값 계산
         int maxSelect = nums.length / 2;
-        int numOfKinds = pocket.size(); // 폰켓몬 종류의 개수
+        int kindsOfPocket = pocket.size();
         
-        // 선택 가능한 폰켓몬 종류의 최댓값 계산
-        if (numOfKinds >= maxSelect) {
+        if(kindsOfPocket > maxSelect) {
             answer = maxSelect;
-        } else {
-            answer = numOfKinds;
         }
-        
+        else {
+            answer = kindsOfPocket;
+        }
         return answer;
     }
 }
