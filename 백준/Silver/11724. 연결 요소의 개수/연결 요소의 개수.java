1f0c0bd025160b25e383517332ffdb3f080a0a33
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2024-08-20 11:32:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 141740 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,34 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static ArrayList<Integer>[] A;
+
     static boolean visited[];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());  // 노드개수
-        int m  = Integer.parseInt(st.nextToken()); // 엣지개수
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(st.nextToken());
 
         A = new ArrayList[n+1];
-        visited  = new boolean[n + 1];
 
-        for(int i=1; i< n+1; i++) {
+        visited = new boolean[n+1];
+        for(int i=1;i<n+1;i++) {
             A[i] = new ArrayList<Integer>();
         }
-        for(int i=0; i < m; i++) {
+
+        for(int i=0; i<m; i++) {
+
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             A[s].add(e);
-            A[e].add(s);  // 연결상태 추가
+            A[e].add(s);
         }
+
         int count = 0;
-        for(int i=1;i<n+1; i++) {
+
+        for(int i=1; i<n+1; i++) {
+
             if(!visited[i]) {
                 count++;
                 DFS(i);
@@ -38,7 +50,7 @@ public class Main {
     }
 
     static void DFS(int v) {
-        if(visited[v]){
+        if(visited[v]) {
             return;
         }
         visited[v] = true;
