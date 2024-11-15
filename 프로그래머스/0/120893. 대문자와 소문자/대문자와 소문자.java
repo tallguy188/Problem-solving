fd8d53313522861d8d54727d10ab5abd976cabc9
ChanHong Min <80328668+tallguy188@users.noreply.github.com>
2024-11-16 04:42:43 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.79 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+       
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : my_string.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+            else sb.append(Character.toUpperCase(c));
+        }
+        String answer = sb.toString();
+        
+        
+        return answer;
+    }
+}
